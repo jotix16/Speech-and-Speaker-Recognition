afec3e83bbf2b86d8e4362f7b20a635b92c1a5aa
Jotix16 <zhobromikel@gmail.com>
2020-04-23 19:44:05 +0200

    Finished lab3

--- Lab3/lab3_proto.py
+++ Lab3/lab3_proto.py
@@ -83,10 +83,15 @@ def hmmLoop(hmmmodels, namelist=None):
        wordLoop = hmmLoop(wordHMMs, ['o', 'z', '1', '2', '3'])
     """
 
-    
 def extract_features_and_targets(filename):
-    """
-    extracts lmfcc, mspecc and targets from a *.wav sound file
+    """ extracts lmfcc, mspecc and targets from a *.wav sound file
+    Args:
+        filename: location of .wav file for which extraction has to take place
+
+    Output:
+        lmfcc: liftered mfcc featues
+        mspec: filterbank featuers
+        targets indexes of the target state for each feature
     """
     phoneHMMs_all = np.load('../Lab2/lab2_models_all.npz',allow_pickle=True)['phoneHMMs'].item()
     nstates, stateList, phones = pickle.load( open('saved_files/phoneHMM_states.pkl', 'rb'))
@@ -104,27 +109,28 @@ def extract_features_and_targets(filename):
     targets = [stateList.index(k) for k in list(np.array(stateTrans)[viterbi_path,])]
     return lmfcc, mspecc, targets
 
-
-
 def create_dynamic_features(data):
-    """
-    data: list of dictionaries with keys: 'lmfcc', 'mspec' and 'targets'
+    """ Creates dynamic features by concatinated 7 lmfcc or mspec featues together for each step
+    Arg
+        data: list of dictionaries with keys: 'lmfcc', 'mspec' and 'targets'
     
-    output: lmfcc_features: [NxD_lmfcc] where N is nr of all concatinated samples of all words in data
-            mspec_features: [NxD_mspec] where N is nr of all concatinated samples of all words in data
-            targets: [N,] index of state for each sample
+    Output
+        lmfcc_features: [NxD_lmfcc*7] where N is nr of all concatinated samples of all words in data
+        mspec_features: [NxD_mspec*7] where N is nr of all concatinated samples of all words in data
+        targets: [N,] index of state for each sample
     """
     
     D_lmfcc = data[0]['lmfcc'].shape[1]
     D_mspec = data[0]['mspec'].shape[1]
-    # Features to be returned
     N = sum([len(x['targets']) for x in data])
-    print(N)
-    lmfcc_x = np.zeros((N,D_lmfcc*7))
-    mspec_x = np.zeros((N,D_mspec*7))
+
+    # Features to be returned
+    lmfcc_features = np.zeros((N,D_lmfcc*7))
+    mspec_features = np.zeros((N,D_mspec*7))
     
     # Targets to be returned
     targets = []
+
     # through all data
     k = 0
     for x in tqdm(data): 
@@ -133,54 +139,72 @@ def create_dynamic_features(data):
         ## for each timestep
         for i in range(times):
             if i<3 or i>=times-3:
-                lmfcc_x[k,:]=np.hstack(np.pad(x['lmfcc'], pad_width=((3, 3), (0, 0)), mode='reflect')[i:i+7,:])
-                mspec_x[k,:]=np.hstack(np.pad(x['mspec'], pad_width=((3, 3), (0, 0)), mode='reflect')[i:i+7,:])
+                lmfcc_features[k,:]=np.hstack(np.pad(x['lmfcc'], pad_width=((3, 3), (0, 0)), mode='reflect')[i:i+7,:])
+                mspec_features[k,:]=np.hstack(np.pad(x['mspec'], pad_width=((3, 3), (0, 0)), mode='reflect')[i:i+7,:])
             else:
-                lmfcc_x[k,:]=np.hstack(x['lmfcc'][i-3:i+4,:])
-                mspec_x[k,:]=np.hstack(x['mspec'][i-3:i+4,:])
+                lmfcc_features[k,:]=np.hstack(x['lmfcc'][i-3:i+4,:])
+                mspec_features[k,:]=np.hstack(x['mspec'][i-3:i+4,:])
             k +=1
-        # lmfcc_x = np.vstack((lmfcc_x, lmfcc_features))
-        #mspec_x = np.vstack((mspec_x, mspec_features))
         targets = targets + x['targets']
-    return lmfcc_x, mspec_x, targets
-
-
-
+    return lmfcc_features, mspec_features, targets
 
 def create_non_dynamic_features(data):
-    """
-    data: list of dictionaries with keys: 'lmfcc', 'mspec' and 'targets'
+    """ Puts lmfcc and mspec features in arrays that can used for training
+    Arg
+        data: list of dictionaries with keys: 'lmfcc', 'mspec' and 'targets'
     
-    output: lmfcc_features: [NxD_lmfcc] where N is nr of all concatinated samples of all words in data
-            mspec_features: [NxD_mspec] where N is nr of all concatinated samples of all words in data
-            targets: [N,] index of state for each sample
+    Output
+        lmfcc_features: [NxD_lmfcc] where N is nr of all concatinated samples of all words in data
+        mspec_features: [NxD_mspec] where N is nr of all concatinated samples of all words in data
+        targets: [N,] index of state for each sample
     """
     
     D_lmfcc = data[0]['lmfcc'].shape[1]
     D_mspec = data[0]['mspec'].shape[1]
-    # Features to be returned
     N = sum([len(x['targets']) for x in data])
-    print(N)
-    lmfcc_x = np.zeros((N,D_lmfcc))
-    mspec_x = np.zeros((N,D_mspec))
-    
+
+    # Features to be returned
+    lmfcc_features = np.zeros((N,D_lmfcc))
+    mspec_features = np.zeros((N,D_mspec))
+
     # Targets to be returned
     targets = []
+
     # through all data
     k = 0
     for x in tqdm(data): 
         times, dim = x['lmfcc'].shape
         ## for each timestep
         for i in range(times):
-            lmfcc_x[k,:]=x['lmfcc'][i,:]
-            mspec_x[k,:]=x['mspec'][i,:]
+            lmfcc_features[k,:]=x['lmfcc'][i,:]
+            mspec_features[k,:]=x['mspec'][i,:]
             k +=1
-        # lmfcc_x = np.vstack((lmfcc_x, lmfcc_features))
-        #mspec_x = np.vstack((mspec_x, mspec_features))
         targets = targets + x['targets']
-    return lmfcc_x, mspec_x, targets
+    return lmfcc_features, mspec_features, targets
 
+def save_predictions(Y_test, Y_pred, featuretype):
+    """ Saves the prediction indexes for Y_test amd Y_pred
 
+    Args
+        Y_test: [NxS] where N is nr of samples and S ist the number of possible outputs
+        Y_pred: [NxS] where N is nr of samples and S ist the number of possible outputs
+    """
+    Y_test_indexes = Y_test.argmax(axis=1)
+    Y_pred_indexes = Y_pred.argmax(axis=1)
+    np.savez('datas/'+featuretype+'_pred.npz', Y_test_indexes=Y_test_indexes, Y_pred_indexes=Y_pred_indexes )
 
+def load_predictions(featuretype):
+    """ Saves the prediction indexes for Y_test amd Y_pred
 
+    Args
+        featuretype: type of feature for which the preadictions should be load.
+
+    Output
+        Y_test_indexes: [N,] where N is nr of test outcomes
+        Y_pred_indexes: [N,] where N is nr of predicted outcomes
+    """
+    with np.load('datas/'+featuretype+'_pred.npz', allow_pickle=True) as data:
+        Y_test_indexes = data['Y_test_indexes']
+        Y_pred_indexes = data['Y_pred_indexes']
+    return Y_test_indexes, Y_pred_indexes
 
